d7831f0b9025a041c21f8d1b706479423dae4442
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-02 21:54:40 +0200

    Redesign API and remove React

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { IPluginApi, IPluginDef, IPluginStore } from './types';
+
+const Store: IPluginStore = {
+  defaultConfigs: {},
+  initialStates: {},
+  initHandlers: {},
+  methodHandlers: {},
+  eventHandlers: {},
+};
+
+export function registerPlugin<PluginConfig extends object, PluginState>(
+  pluginDef: IPluginDef<PluginConfig, PluginState>,
+): IPluginApi<PluginConfig, PluginState> {
+  const { name: pluginName, defaultConfig, initialState } = pluginDef;
+
+  Store.defaultConfigs[pluginName] = defaultConfig;
+  Store.initialStates[pluginName] = initialState;
+
+  return {
+    init: handler => {
+      registerPluginHandler(Store.initHandlers, pluginName, handler);
+    },
+    method: (methodName, handler) => {
+      registerPluginHandler(Store.methodHandlers, pluginName, {
+        methodName,
+        handler,
+      });
+    },
+    on: (eventName, handler) => {
+      registerPluginHandler(Store.eventHandlers, pluginName, {
+        eventName,
+        handler,
+      });
+    },
+  };
+}
+
+function registerPluginHandler<T>(
+  handlers: { [pluginName: string]: T[] },
+  pluginName: string,
+  entry: T,
+) {
+  if (!handlers[pluginName]) {
+    handlers[pluginName] = [];
+  }
+
+  handlers[pluginName].push(entry);
+}
+
+// Test usage
+
+const { init, method, on } = registerPlugin({
+  name: 'test-plugin',
+  defaultConfig: { enabled: false },
+  initialState: { name: 'Ron' },
+});
+
+init(({ getState }) => {
+  // console.log(getState().name);
+});
+
+method('doIt', ({ setState }) => {
+  setState(() => ({ name: 'Ronnie' }));
+});
+
+on('other-plugin.didSomething', ({ getConfig }) => {
+  // console.log(getConfig().enabled);
+});
