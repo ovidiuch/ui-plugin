b59d38afd38875b2d8f71ae7898ba9e7b1132e64
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-04 18:02:42 +0200

    Clean up excessive typing

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -1,19 +1,6 @@
 import { getPluginStore } from './pluginStore';
 import { EventHandler, InitHandler, MethodHandler } from './shared';
 
-interface IPluginApi<PluginConfig extends object, PluginState> {
-  init: (handler: InitHandler<PluginConfig, PluginState>) => void;
-  method: (
-    methodName: string,
-    handler: MethodHandler<PluginConfig, PluginState>,
-  ) => void;
-  on: (
-    eventPath: string,
-    handler: EventHandler<PluginConfig, PluginState>,
-  ) => void;
-  // TODO: onStateChange
-}
-
 interface IPluginDef<PluginConfig extends object, PluginState> {
   name: string;
   defaultConfig?: PluginConfig;
@@ -22,7 +9,7 @@ interface IPluginDef<PluginConfig extends object, PluginState> {
 
 export function registerPlugin<PluginConfig extends object, PluginState>(
   pluginDef: IPluginDef<PluginConfig, PluginState>,
-): IPluginApi<PluginConfig, PluginState> {
+) {
   const { name, defaultConfig = {}, initialState } = pluginDef;
 
   const {
@@ -38,15 +25,27 @@ export function registerPlugin<PluginConfig extends object, PluginState>(
     initialState,
   });
 
+  function init(handler: InitHandler<PluginConfig, PluginState>) {
+    addInitHandler({ pluginName: name, handler });
+  }
+
+  function method(
+    methodName: string,
+    handler: MethodHandler<PluginConfig, PluginState>,
+  ) {
+    addMethodHandler({ pluginName: name, methodName, handler });
+  }
+
+  function on(
+    eventPath: string,
+    handler: EventHandler<PluginConfig, PluginState>,
+  ) {
+    addEventHandler({ pluginName: name, eventPath, handler });
+  }
+
   return {
-    init: handler => {
-      addInitHandler({ pluginName: name, handler });
-    },
-    method: (methodName, handler) => {
-      addMethodHandler({ pluginName: name, methodName, handler });
-    },
-    on: (eventPath, handler) => {
-      addEventHandler({ pluginName: name, eventPath, handler });
-    },
+    init,
+    method,
+    on,
   };
 }
