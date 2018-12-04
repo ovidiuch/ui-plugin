091bbb2a50c134484017c1fc4f6fac3e83cfa967
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-04 16:59:02 +0200

    Remove unused file

--- src/types/PluginScope.ts
+++
@@ -1,8 +0,0 @@
-import { IPluginConfigs, IPluginStates } from './shared';
-
-// TODO: Remove this type
-export interface IPluginScope {
-  unmounted: boolean;
-  config: IPluginConfigs;
-  state: IPluginStates;
-}
