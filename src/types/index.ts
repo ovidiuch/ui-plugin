0e9c82da943d2f8a330efa414116c0be93068c50
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-04 17:21:40 +0200

    Remove unused export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
 export { IPluginApi } from './PluginApi';
 export { IPluginContext } from './PluginContext';
-export { IPluginScope } from './PluginScope';
 export { IPluginStore } from './PluginStore';
 export { IPluginConfigs, IPluginStates } from './shared';
