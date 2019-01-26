999e01d4a8443fd92d7b7d4067dc923929d8d647
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-26 16:48:02 +0200

    Update registerPlugin.ts

--- src/NEW/registerPlugin.ts
+++ src/NEW/registerPlugin.ts
@@ -1,8 +1,9 @@
-import { IPluginSpec, IPluginDef } from './IPluginSpec';
+import { IPluginSpec } from './IPluginSpec';
 import { addPluginDef } from './pluginStore';
 
-export function registerPlugin<PluginSpec extends IPluginSpec>(
-  pluginDef: IPluginDef,
-) {
+export function registerPlugin<PluginSpec extends IPluginSpec>(pluginDef: {
+  name: PluginSpec['name'];
+  methods: PluginSpec['methods'];
+}) {
   addPluginDef(pluginDef);
 }
