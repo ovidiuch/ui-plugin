732cf4fed7cd4279fe0e42883c83b1f1ad961236
Ovidiu Cherecheș <skidding@gmail.com>
2021-02-16 15:57:06 +0200

    Update createPluginContext.ts

--- src/createPluginContext.ts
+++ src/createPluginContext.ts
@@ -15,7 +15,7 @@ import { SharedPluginContext } from './types/SharedPluginContext';
 
 // Why are contexts and methods cached? Because they are passed down to
 // components and used as dependencies for child callbacks and effects (eg.
-// React Hooks). By reusing methods handlers at the plugin system level,
+// React Hooks). By reusing method handlers at the plugin system level,
 // we enable downstream memoization at any level.
 export function createPluginContext<T extends PluginSpec>(
   pluginName: T['name'],
